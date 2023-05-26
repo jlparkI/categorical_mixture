20718cfa402ca07ab9e30642c8279dac69365a28
jlparkinson1 <jlparkinson1@gmail.com>
2023-05-26 15:27:09 -0700

    Updated the setup, changed file names to multinoulli_mix* since this is a more accurate representation of what the model is / does.

---
+++ multinoulli_mixture/__init__.py
@@ -0,0 +1,3 @@
+#Version number. Updated if generating a new
+#release, otherwise, do not change.
+__version__ = "0.0.0.1"
--- setup.py
+++ setup.py
@@ -1,53 +1,61 @@
+"""Setup for the """
+import os
 from setuptools import setup, find_packages
-import os, numpy, platform, subprocess
+import numpy
 from Cython.Distutils import build_ext
+#Irrespective of what your linter says, do not
+#move this import above setuptools; this import
+#monkey patches setuptools, and -- remarkably --
+#reversing the order will lead to an error.
 from distutils.extension import Extension
 
-if "CUDA_PATH" in os.environ:
-    CUDA_PATH = os.environ["CUDA_PATH"]
-else:
-    CUDA_PATH = "/usr/local/cuda"
+
+def get_version(start_fpath):
+    """Retrieves the version number."""
+    os.chdir(os.path.join(start_fpath, "multinoulli_mixture"))
+    with open("__init__.py", "r") as fhandle:
+        version_line = [l for l in fhandle.readlines() if
+                l.startswith("__version__")]
+        version = version_line[0].split("=")[1].strip().replace('"', "")
+    os.chdir(start_fpath)
+    return version
 
 
-abspath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "multinomial_mix")
+
+setup_fpath = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+readme_path = os.path.join(setup_fpath, "README.md")
+
+with open(readme_path, "r") as fhandle:
+    long_description = "".join(fhandle.readlines())
+
+extension_path = os.path.join(setup_fpath, "multinoulli_mixture",
+        "core_cpu_funcs")
 cpu_wrappers = Extension("core_cpu_func_wrappers",
-        sources = [os.path.join(abspath, "core_cpu_func_wrappers.pyx"),
-            os.path.join(abspath, "prob_calcs.c")],
-        language="c",
-        include_dirs=[numpy.get_include(), abspath],
-        library_dirs=[abspath],
+        sources = [os.path.join(extension_path, "core_cpu_func_wrappers.pyx"),
+            os.path.join(extension_path, "responsibility_calcs.cpp"),
+            os.path.join(extension_path, "weighted_counts.cpp")],
+        language="c++",
+        include_dirs=[numpy.get_include(), extension_path],
         extra_compile_args=["-O3"])
 
 cpu_wrappers.cython_directives = {"language_level":"3"}
 
-#cuda_wrappers = Extension("core_cuda_func_wrappers",
-#        sources = ["core_cuda_func_wrappers.pyx"],
-#        language="c++",
-#        libraries = ["gpu_prob_calcs", "cudart_static", "cuda"],
-#        include_dirs=[numpy.get_include(),
-#            os.path.dirname(os.path.abspath((__file__)))],
-#        library_dirs=[os.path.dirname(os.path.abspath((__file__))),
-#                    os.path.join(CUDA_PATH, "lib64")],
-#        extra_link_args = ["-lrt"]
-#        )
-
-#cuda_wrappers.cython_directives = {"language_level":"3"}
-
-
-
 setup(
-        name="multinomial_mix",
-        version="0.0.0.1",
+        name="multinoulli_mix",
+        version=get_version(setup_fpath),
         packages=find_packages(),
         cmdclass = {"build_ext": build_ext},
         author="Jonathan Parkinson",
         author_email="jlparkinson1@gmail.com",
+        description = "Fitting a mixture of multinoullis model to sequence data",
+        long_description = long_description,
+        long_description_content_type = "text/markdown",
+        install_requires = ["numpy>=1.10", "scipy>=1.7.0",
+            "cython>=0.10"],
         ext_modules = [cpu_wrappers],
-        package_data={"": ["*.h", "*.c", "*.cu",
+        package_data={"": ["*.h", "*.c", "*.cpp",
             "*.pyx", "*.sh"]}
     )
 
-#os.remove("core_cuda_func_wrappers.cpp")
-#os.remove("core_cpu_func_wrappers.c")
-#os.remove("libprob_calcs.a")
-#os.remove("prob_calcs.o")
+os.chdir(extension_path)
+os.remove("core_cpu_func_wrappers.cpp")
